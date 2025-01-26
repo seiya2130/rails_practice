357311d05fdbe94b9670521a39d08237c89f46b3
SeiyaSatoh <satohseiya0312@gmail.com>
2025-01-26 18:37:07 +0900

    feat: personテスト追加

---
+++ app/models/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+    def initialize(name = 'no name', age = 0)
+        @name = name
+        @age = age
+    end
+
+    def name
+        @name
+    end
+
+    def age
+        @age
+    end
+end
